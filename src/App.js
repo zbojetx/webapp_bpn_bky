f84325ec0704bbf89be8dd3318e74360b2cf2791
Zul Amri Durrin Nafis <zulamridn@gmail.com>
2021-02-10 08:25:36 +0700

    first init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import { Collapse, Space } from 'antd';
+const { Panel } = Collapse;
+
+const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`;
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +27,19 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Space direction="vertical">
+        <Collapse collapsible="header" defaultActiveKey={['1']}>
+          <Panel header="This panel can only be collapsed by clicking text" key="1">
+            <p>{text}</p>
+          </Panel>
+        </Collapse>
+        <Collapse collapsible="header" style={{borderColor: '#e67e22'}}>
+          <Panel header="This panel can't be collapsed" key="1" style={{backgroundColor:'#f0932b', borderColor: '#e67e22'}}>
+            <p>{text}</p>
+          </Panel>
+        </Collapse>
+      </Space>
     </div>
   );
 }
